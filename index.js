92ecfb2001284f917735ce8e97dae04b480c5e6b
graanco <benjamin.a.wells@me.com>
2019-06-13 15:17:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,13 @@ class SimpliSafe {
     platform.config = config;
     platform.accessories = {};
     ss = new API(config.SerialNumber, config.username);
+    platform.refreshing_Sensors_Timer = 0;
+    platform.refreshing_Sensors = false;
 
     if (api) {
       platform.api = api;
       platform.api.on('didFinishLaunching', function() {
-          if (platform.config.reset) {
+        if (platform.config.reset) {
           var fs = require('fs');
           var cfg = JSON.parse(fs.readFileSync(User.configPath()));
           var nPlatforms=[];
@@ -43,16 +45,23 @@ class SimpliSafe {
         }
 
         if (platform.config.password){
-          ss.login_via_credentials(config.password)
+          ss.apiconfig()
           .then(function(){
-            return platform.initPlatform(false);
+            ss.login_via_credentials(config.password)
+            .then(function(){
+              if (!platform.config.refresh_token) platform.tokenCheck();
+              return platform.initPlatform(false);
+            });
           });
         } else {
-          ss.login_via_token(config.refresh_token)
+          ss.apiconfig()
           .then(function(){
-            return platform.initPlatform(false);
+            ss.login_via_token(config.refresh_token)
+            .then(function(){
+              return platform.initPlatform(false);
+            });
           });
-        }
+        };
 
         platform.log("Up and monitoring.");
 
@@ -60,30 +69,23 @@ class SimpliSafe {
     };
   };//End of SimpliSafe Function
 
-  initPlatform(cached = true) {
+  async initPlatform() {
     var platform = this;
-    return ss.get_Sensors(cached)
-      .then(function () {
-        var system = ss.sensors;
-        //Add the security alarm system as a sensor;
-        system[platform.config.SerialNumber] = {'type': ss.SensorTypes.SecuritySystem, 'serial': platform.config.SerialNumber, 'name': 'SimpliSafe Alarm System'}
-        Object.keys(system).forEach(sensor=> {
-          //found Accessory to Sensor return to continue searching and change Reachability to true
-          if (platform.accessories[sensor]) return platform.accessories[sensor].updateReachability(true);
-            //found new sensor add it as an accessory
-            if (ss.sysVersion == 3) {
-              if (![ss.SensorTypes.CarbonMonoxideSensor, ss.SensorTypes.ContactSensor, ss.SensorTypes.GlassBreakSensor, ss.SensorTypes.LeakSensor, ss.SensorTypes.MotionSensor, ss.SensorTypes.SecuritySystem, ss.SensorTypes.SmokeSensor, ss.SensorTypes.TemperatureSensor].includes(ss.sensors[sensor].type)) return;
-              platform.addAccessory(platform.createAccessory(sensor), true);
-            } else {
-              if (![ss.SensorTypes.ContactSensor, ss.SensorTypes.SecuritySystem, ss.SensorTypes.TemperatureSensor].includes(ss.sensors[sensor].type)) return;
-              platform.addAccessory(platform.createAccessory(sensor), true);
-            };
-
-        });
-      })
-      .catch(err=>{
-        platform.log(err);
-      });
+    var system = ss.sensors;
+    //Add the security alarm system as a sensor;
+    system[platform.config.SerialNumber] = {'type': ss.SensorTypes.SecuritySystem, 'serial': platform.config.SerialNumber, 'name': 'SimpliSafe Alarm System'}
+    Object.keys(system).forEach(sensor=> {
+      //found Accessory to Sensor return to continue searching and change Reachability to true
+      if (platform.accessories[sensor]) return platform.accessories[sensor].updateReachability(true);
+        //found new sensor add it as an accessory
+        if (ss.sysVersion == 3) {
+          if (![ss.SensorTypes.CarbonMonoxideSensor, ss.SensorTypes.ContactSensor, ss.SensorTypes.GlassBreakSensor, ss.SensorTypes.LeakSensor, ss.SensorTypes.MotionSensor, ss.SensorTypes.SecuritySystem, ss.SensorTypes.SmokeSensor, ss.SensorTypes.TemperatureSensor].includes(ss.sensors[sensor].type)) return;
+          platform.addAccessory(platform.createAccessory(sensor), true);
+        } else {
+          if (![ss.SensorTypes.ContactSensor, ss.SensorTypes.SecuritySystem, ss.SensorTypes.TemperatureSensor].includes(ss.sensors[sensor].type)) return;
+          platform.addAccessory(platform.createAccessory(sensor), true);
+        };
+    });
   };// End of initPlatform Function
 
   tokenCheck(){
@@ -191,7 +193,7 @@ class SimpliSafe {
             .getCharacteristic(Characteristic.SecuritySystemTargetState)
             .on('get', (callback)=>platform.getAlarmState(callback))
             .on('set', (state,callback)=>{
-                platform.setAlarmtoState(state,callback);
+                platform.setAlarmState(state,callback);
                 accessory.getService(Service.SecuritySystem)
                   .setCharacteristic(Characteristic.SecuritySystemCurrentState, state);
             });
@@ -217,7 +219,7 @@ class SimpliSafe {
       }
   }// End Of Function addAccessory
 
-  getCurrentState(callback) {
+  async getCurrentState(callback) {
     var platform = this;
     ss.get_Alarm_State()
     .then(function(state) {
@@ -245,13 +247,18 @@ class SimpliSafe {
 
   async getState(SerialNumber, callback) {
     var platform = this;
-    if (!ss.refreshing_Sensors && ss.refreshing_Sensors_Timer + (platform.config.refresh_timer * 1000) <= Date.now())  {
-      if (!platform.config.refresh_token) platform.tokenCheck();
-      platform.log('Refreshing Sensor Data');
-      await ss.get_Sensors(false);
+    var count=0; // failout just incase....
+    if (!ss.refreshing_Sensors){
+      if ((platform.refreshing_Sensors_Timer + (platform.config.refresh_timer * 1e3)) <= Date.now()) {
+        platform.log('Refreshing Sensors Data');
+        await ss.get_Sensors(false);
+        platform.refreshing_Sensors_Timer = Date.now();
+      };
     }
+
     var refreshing = setInterval(()=>{
-      if (!ss.refreshing_Sensors) {
+      if ((platform.config.refresh_timer*2)>=count) ss.refreshing_Sensors = false; else count ++;
+      if (ss.refreshing_Sensors==false) {
         clearInterval(refreshing);
         if (platform.accessories[SerialNumber].getService(Service.TemperatureSensor)) {
           callback(null, (ss.sensors[SerialNumber].temp-32) * 5/9);
@@ -259,10 +266,10 @@ class SimpliSafe {
           callback(null, ss.sensors[SerialNumber]['status']['triggered']);
         };
       };
-    }, 1000);
+    }, 500);
   };// End of Function getState
 
-  getAlarmState(callback) {
+  async getAlarmState(callback) {
     var platform = this;
     ss.get_Alarm_State()
     .then(function(state) {
@@ -285,7 +292,7 @@ class SimpliSafe {
       });
   };// End of Function getAlarmState
 
-  setAlarmState(state, callback) {
+  async setAlarmState(state, callback) {
     // Set state in simplisafe 'off' or 'home' or 'away'
     var platform = this;
     var ssState;
@@ -310,3 +317,8 @@ class SimpliSafe {
   };// End of Function setAlarmState
 
 }; // End Of Class
+// Sample function to show how developer can remove accessory dynamically from outside event
+// Need to look up Accessoy Removal process....
+//  this.api.unregisterPlatformAccessories("homebridge-platform-simplisafe", "homebridge-platform-simplisafe", this.accessories);
+
+//  this.accessories = [];
