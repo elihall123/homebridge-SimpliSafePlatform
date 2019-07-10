493e4f4aa77dc160627be66ab7c01f34ad94de03
graanco <benjamin.a.wells@me.com>
2019-07-10 10:27:05 -0400

    Update api.js

--- client/api.js
+++ client/api.js
@@ -1,20 +1,7 @@
 var websession = require('https');
 var fs = require('fs');
 var vm = require('vm');
-
-
-function BasicAuth(login, password){
-  return "Basic " +  Buffer.from(login + ':' + password).toString('base64');
-}
-var g = "4df55627-46b2-4e2c-866b-1521b395ded2",
-    h = "WebApp";
     
-function uuid4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-};
 
 var _access_token;
 var _access_token_expire;
@@ -23,19 +10,60 @@ var _email;
 
 module.exports = class API {
   //Class SimpliSafe API
+  async _Authenticate(payload_data){
+    //Request token data...
+    var self = this;
+    var resp = await self.request({
+      method:'POST',
+      endpoint:'api/token',
+      data: payload_data,
+      auth: "Basic: " + Buffer.from(self.uuid + ".2074.0.0.com.simplisafe.mobile" + ':').toString('base64'),
+    });
+
+    _access_token = resp.access_token;
+    _access_token_expire = Date.now() + ((resp.expires_in-60) * 1000);
+    _access_token_type = resp.token_type;
+    this._refresh_token = resp.refresh_token;
+  };//End Of Function _authenticate
+
+  async _get_User_ID (){
+    var self = this;
+    var resp = await self.request({method:'GET',endpoint: 'api/authCheck'})
+    this.user_id = resp['userId'];
+  };//End Of Function _getUserId
+
+  async _Refresh_Access_Token(refresh_token){
+    //Regenerate an access token.
+    var self = this;
+    self.log("Regenerating Access Token");
+    await self._Authenticate({'grant_type': 'refresh_token', 'username': _email, 'refresh_token': refresh_token,})
+    self._actively_refreshing = false;
+  };//End Of Function _refresh_access_token
+
+  async API_Config() {
+    var self = this;
+    var resp = await webResponse(new URL('https://webapp.simplisafe.com/ssAppConfig.js'), {METHOD: 'GET'})
+    resp = resp.body.replace('})(window);', 'return g;});')
+                .replace('var a=', 'var a=g.')
+                .replace(';', '');
+    let APICFGS = vm.runInThisContext(resp);
+    APICFGS(self);
+  };//End Of Function API_Config
+
   constructor(SerialNumber, email, log) {
     //Initialize.
     var self = this;
-    self.log = log;
+    if (!log) {self.log = console.log;} else {self.log = log;}
     _email = email;
     self.refresh_token_dirty = false;
     self.serial = SerialNumber;
     self.user_id;
+    self.uuid = uuid4();
     self._refresh_token = '';
     self.sensors = {};
     self._actively_refreshing = false;
-    self.refreshing_Sensors = false;
     self.cameras = {};
+    self.readied = false;
     self.SensorTypes = {
       /*Commented out sensors not used by Homebridge and yes I know the siren could be a speaker*/
       0:'SecuritySystem',
@@ -67,39 +95,119 @@ module.exports = class API {
       'unknown': 99*/
     };
 
-  };//end of constructor
+  };//End Of Function Constructor
 
-  async apiconfig() {
+  async get_Alarm_State() {
     var self = this;
-    var resp = await webResponse(new URL('https://webapp.simplisafe.com/ssAppConfig.js'), {METHOD: 'GET'})
-    resp = resp.body.replace('})(window);', 'return g;});')
-                .replace('var a=', 'var a=g.')
-                .replace(';', '');
-    let APICFGS = vm.runInThisContext(resp);
-    APICFGS(self);
-  }
+    return await self.get_System();
+  };//End Of Function get_Alarm_State
 
-  getClientId() {
-    try {
-        var n = [];
-        n.push(g), n.push("1.30.1".replace(/\./g, "-")), n.push(h);
-        return n.join(".") + ".simplisafe.com";
-    } catch (e) {
-        this.log(e)
+  async get_Camera_Stream(uuid){
+    var self = this;
+    if (!self.simplisafe) await self.API_Config();
+    if (_access_token_expire && Date.now() >= _access_token_expire && this._actively_refreshing == false){
+            this._actively_refreshing = true;
+            await this._refresh_access_token(this._refresh_token);
     }
-  }
 
-  getDeviceId() {
-    try {
-        var n = (new Date, 'useragent="2105 CFNetwork/902.2 Darwin/17.7.0"');
-        n += '; uuid="' + g + '"';
-        var r = h + "; " + n;
-        return r
-    } catch (e) {
-        this.log(e)
+    var url = new URL(self.simplisafe.webapp.mediaHost + self.simplisafe.webapp.mediaPath + '/' + uuid + '/flv');
+
+    headers={
+      Authorization: _access_token_type + ' ' + _access_token,
+      Accept: application/json
+    };
+
+    var options = {
+      method: method,
+      headers: headers
     }
-  }
+    var resp = await webResponse(url, options, data);
+    return resp.body;
 
+  };//End Of Function get_Camera_Stream
+
+  async get_System(){
+    //Get systems associated to this account.
+    var self = this;
+    try{
+      var resp = await await this.request({method: 'GET', endpoint: 'users/' + self.user_id + '/subscriptions', params: {'activeOnly': 'true'}});//this.get_Subscription_Data();
+      if (resp >= 400) throw('Access Forbidden. Please wait an hour and try again. Error: ', resp);
+      for (var system_data of resp.subscriptions){
+        if (system_data.location.system.serial === self.serial) {
+          //self.log(system_data.features, system_data.location);
+            self.subId = system_data.sid;
+            self.sysVersion = system_data.location.system.version;
+            for (var camera of system_data.location.system.cameras){
+              self.cameras[camera.uuid] = camera;
+            }
+            return system_data.location.system;
+          }
+      };
+    } catch (e) {
+            self.log('Get_System',e);
+            return false;
+    };
+  };//End Of Function get_System
+
+  async get_Sensors(cached = true) {
+    var self = this;
+    if (self.sysVersion==3) {
+      var parsedBody = await self.request({
+        method:'GET',
+        endpoint:'ss3/subscriptions/' + self.subId + '/sensors',
+        params:{'forceUpdate': (cached==false).toString().toLowerCase()} //false = coming from cache
+      })
+      //Check for a successful refresh on sensors --- on 409 send old data
+      if (!parsedBody.success) return self.sensors;
+      for (var sensor_data of parsedBody.sensors) {
+          self.sensors[sensor_data['serial']] = sensor_data;
+          if (sensor_data.type == self.SensorTypes['ContactSensor']) {
+            self.sensors[sensor_data['serial']] = {...sensor_data, 'entryStatus' : sensor_data.status.triggered ? 'open' : 'closed'};
+          } else {
+            self.sensors[sensor_data['serial']] = sensor_data;
+          }
+      }
+    } else {
+      var parsedBody = await self.request({
+          method:'GET',
+          endpoint: 'subscriptions/' + self.subId + '/settings',
+          params:{'settingsType': 'all', 'cached': cached.toString().toLowerCase()} //true = coming from cache
+      })
+          //Check for a successful refresh on sensors --- on 409 send old data
+      if (!parsedBody.success) return self.sensors;
+      for (var sensor_data of parsedBody.settings.sensors) {
+        if (!sensor_data['serial']) break;
+          if (sensor_data.type == self.SensorTypes['ContactSensor']) {
+            self.sensors[sensor_data['serial']] = {...sensor_data, 'status' : { triggered : sensor_data.entryStatus=='open' }};
+          } else {
+            self.sensors[sensor_data['serial']] = sensor_data;
+          }
+      };
+    };
+  };//End Of Function get_Sensors
+
+  async login_via_credentials(password){
+    //Create an API object from a email address and password.
+      var self = this;
+      await self._Authenticate({'grant_type': 'password', 'username': _email, 'password': password});
+      await self._get_User_ID();
+      await self.get_System();
+      await self.get_Sensors();
+      self.readied = true;
+      //self.websocket();
+      return;
+  };//End Of Function login_via_credentials
+  
+  ssEvent(){
+    var self = this;
+    self.log(e);
+  };//End Of Function ssEvent
+
+  ssHiddenEvent(){
+    var self = this;
+    self.log(e);
+  };//End Of Function ssHiddenEvent
+  
   async websocket(){
     var self = this;
     try {
@@ -150,157 +258,32 @@ module.exports = class API {
     } catch (e) {
         self.log('websocket ',e);
     }
-  }
+  };//End Of Function websocket
   
-  ssEvent(){
+  async request({method='', endpoint='', headers={}, params={}, data={}, json={}, ...kwargs}){
     var self = this;
-    self.log(e);
-  }
+    if (!self.simplisafe) await self.API_Config();
+    var refreshing = await setInterval(()=>{if (!self._actively_refreshing)  clearInterval(refreshing);}, 500);
 
-  ssHiddenEvent(){
-    var self = this;
-    self.log(e);
-  }
-  async login_via_credentials(password){
-  //Create an API object from a email address and password.
-    var self = this;
-    await self._authenticate({
-           'grant_type': 'password',
-           'username': _email,
-           'password': password,
-           'device_id' : self.getDeviceId()
-    });
-    await self._get_user_ID();
-    await self.get_system();
-    await self.get_Sensors();
-    //self.websocket();
-    return;
-  };//end of function login_via_credentials
-
-  async login_via_token(refresh_token){
-    //Create an API object from a refresh token.
-    var self = this;
-    await self._refresh_access_token(refresh_token);
-    await self._get_user_ID();
-    await self.get_system();
-    await self.get_Sensors();
-    //self.websocket();
-    return;
-  };//end of function login_via_token
-
-  async _authenticate(payload_data){
-    //Request token data...
-    var self = this;
-    var resp = await self.request({
-      method:'POST',
-      endpoint:'api/token',
-      data: payload_data,
-      auth: "Basic: " + Buffer.from(self.getClientId() + ':').toString('base64')
-    });
-
-    _access_token = resp.access_token;
-    _access_token_expire = Date.now() + ((resp.expires_in-60)*1000);
-    _access_token_type = resp.token_type;
-    this._refresh_token = resp.refresh_token;
-  };//End of function _authenticate
-
-  async _get_user_ID (){
-    var self = this;
-    var resp = await self.request({method:'GET',endpoint: 'api/authCheck'})
-    this.user_id = resp['userId'];
-  };//End of function _getUserId
-
-  async _refresh_access_token(refresh_token){
-    //Regenerate an access token.
-    var self = this;
-    await self._authenticate({
-        'grant_type': 'refresh_token',
-        'username': _email,
-        'refresh_token': refresh_token
-    })
-    self._actively_refreshing = false;
-  };//End of function _refresh_access_token
-
-  async get_system(){
-    //Get systems associated to this account.
-    var self = this;
-    try{
-      var resp = await this.get_subscription_data();
-      if (resp === 403) throw('Access Forbidden. Please wait an hour and try again.');
-      for (var system_data of resp.subscriptions){
-        if (system_data.location.system.serial === self.serial) {
-            self.subId = system_data.sid;
-            self.sysVersion = system_data.location.system.version;
-            for (var camera of system_data.location.system.cameras){
-              self.cameras[camera.uuid] = camera;
-            }
-            return system_data.location.system;
-          }
-      };
-    } catch (e) {
-            self.log(e);
-            return false;
-    };
-  };//End of function get_system
-
-  async get_subscription_data(){
-    var self = this;
-    //Get the latest location-level data.
-    return await this.request({method: 'GET', endpoint: 'users/' + self.user_id + '/subscriptions', params: {'activeOnly': 'true'}});
-  };//End of function get_subscription_data
-
-  async get_Sensors(cached = true) {
-    var self = this;
-    self.refreshing_Sensors = true;
-    if (self.sysVersion==3) {
-      var parsedBody = await self.request({
-        method:'GET',
-        endpoint:'ss3/subscriptions/' + self.subId + '/sensors',
-        params:{'forceUpdate': (cached==false).toString().toLowerCase()} //false = coming from cache
-      })
-      //Check for a successful refresh on sensors --- on 409 send old data
-      if (!parsedBody.success) return self.sensors;
-      for (var sensor_data of parsedBody.sensors) {
-          self.sensors[sensor_data['serial']] = sensor_data;
-          if (sensor_data.type == self.SensorTypes['ContactSensor']) {
-            self.sensors[sensor_data['serial']] = {...sensor_data, 'entryStatus' : sensor_data.status.triggered ? 'open' : 'closed'};
-          } else {
-            self.sensors[sensor_data['serial']] = sensor_data;
-          }
-      }
-    } else {
-      var parsedBody = await self.request({
-          method:'GET',
-          endpoint: 'subscriptions/' + self.subId + '/settings',
-          params:{'settingsType': 'all', 'cached': cached.toString().toLowerCase()} //true = coming from cache
-      })
-          //Check for a successful refresh on sensors --- on 409 send old data
-      if (!parsedBody.success) return self.sensors;
-      for (var sensor_data of parsedBody.settings.sensors) {
-        if (!sensor_data['serial']) break;
-          if (sensor_data.type == self.SensorTypes['ContactSensor']) {
-            self.sensors[sensor_data['serial']] = {...sensor_data, 'status' : { triggered : sensor_data.entryStatus=='open' }};
-          } else {
-            self.sensors[sensor_data['serial']] = sensor_data;
-          }
-      }
-    };
-    self.refreshing_Sensors = false;
-  };//End of function get_Sensors
-
-  async get_CameraStream(uuid){
-    var self = this;
-    if (!self.simplisafe) await self.apiconfig();
-    if (_access_token_expire && Date.now() >= _access_token_expire && this._actively_refreshing == false){
-            this._actively_refreshing = true;
-            await this._refresh_access_token(this._refresh_token);
+    if (_access_token_expire && Date.now() >= _access_token_expire && !this._actively_refreshing){
+      self._actively_refreshing = true;
+      await self._Refresh_Access_Token(this._refresh_token);
     }
 
-    var url = new URL(self.simplisafe.webapp.mediaHost + self.simplisafe.webapp.mediaPath + '/' + uuid + '/flv');
+    var url = new URL(self.simplisafe.webapp.apiHost + self.simplisafe.webapp.apiPath + '/' + endpoint);
+
+    if (params){
+      Object.keys(params).forEach(item=> {
+          url.searchParams.append(item.toString(), params[item]);
+      });
+    };
+
+    if (!kwargs.auth) headers['Authorization'] = _access_token_type + ' ' + _access_token; else headers['Authorization'] = kwargs.auth;
 
     headers={
-      Authorization: _access_token_type + ' ' + _access_token,
-      Accept: application/json
+            ...headers,
+            'Content-Type': 'application/json; charset=utf-8',
+            "User-Agent": "SimpliSafe/2105 CFNetwork/902.2 Darwin/17.7.0"
     };
 
     var options = {
@@ -308,14 +291,13 @@ module.exports = class API {
       headers: headers
     }
     var resp = await webResponse(url, options, data);
-    return resp.body;
-
-  }
-
-  async get_Alarm_State() {
-    var self = this;
-    return await self.get_system();
-  };//End of function get_Alarm_State
+    self.cookie = resp.cookie;
+    if (resp.statusCode >= 400) {
+      return resp.statusCode;
+    } else {
+      return resp.body;
+    }
+  };//End Of Function Request
 
   async set_Alarm_State(value) {
   var self = this;
@@ -331,45 +313,16 @@ module.exports = class API {
         params:{'state': value}
       })
     };
-  };//End of function set_Alarm_State
+  };//End Of Function set_Alarm_State
 
-  async request({method='', endpoint='', headers={}, params={}, data={}, json={}, ...kwargs}){
-    var self = this;
-    if (!self.simplisafe) await self.apiconfig();
-    if (_access_token_expire && Date.now() >= _access_token_expire && this._actively_refreshing == false){
-            this._actively_refreshing = true;
-            await this._refresh_access_token(this._refresh_token);
-    }
+};//End Of Class API
 
-    var url = new URL(self.simplisafe.webapp.apiHost + self.simplisafe.webapp.apiPath + '/' + endpoint);
-
-    if (params){
-      Object.keys(params).forEach(item=> {
-          url.searchParams.append(item.toString(), params[item]);
-      });
-    };
-
-    if (!kwargs.auth) headers['Authorization'] = _access_token_type + ' ' + _access_token; else headers['Authorization'] = kwargs.auth;
-
-    headers={
-            ...headers,
-            'Content-Type': 'application/json; charset=utf-8'
-    };
-
-    var options = {
-      method: method,
-      headers: headers
-    }
-    var resp = await webResponse(url, options, data);
-    self.cookie = resp.cookie;
-    if (resp.statusCode >= 400) {
-      return resp.statusCode;
-    } else {
-      return resp.body;
-    }
-  };//End of function Request
-
-};//end of Class API
+function uuid4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+};
 
 async function webResponse(url, options, data){
   return new Promise(async (resolve, reject) => {
@@ -417,4 +370,4 @@ async function webResponse(url, options, data){
     if (data) req.write(JSON.stringify(data));
     req.end();
   });
-}; //end of function webResponse
+};//End Of Function webResponse
